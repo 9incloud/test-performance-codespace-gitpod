50cf139e0d7848f60b23af794020dbd18aac6255
Chris Yang <kimisme9386@gmail.com>
2021-09-09 11:14:32 +0800

    feat: init project

--- .projenrc.js
+++ .projenrc.js
@@ -1,14 +1,28 @@
-const { AwsCdkTypeScriptApp } = require('projen');
+const { AwsCdkTypeScriptApp, DependenciesUpgradeMechanism } = require('projen');
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.95.2',
+  cdkVersion: '1.121.0',
+  cdkVersionPinning: true,
   defaultReleaseBranch: 'main',
   name: 'test-performance-codespace-gitpod',
-
-  // cdkDependencies: undefined,  /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
-  // deps: [],                    /* Runtime dependencies of this module. */
-  // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                 /* Build dependencies for this module. */
-  // packageName: undefined,      /* The "name" in package.json. */
-  // release: undefined,          /* Add release management to this project. */
+  cdkDependencies: [
+    '@aws-cdk/aws-lambda', '@aws-cdk/aws-s3',
+  ],
+  releaseWorkflow: false,
+  buildWorkflow: false,
+  stale: false,
+  depsUpgrade: DependenciesUpgradeMechanism.NONE,
 });
+
+
+const common_exclude = [
+  'cdk.out',
+  'cdk.context.json',
+  'yarn-error.log',
+  'dependabot.yml',
+  '.idea',
+];
+
+project.gitignore.exclude(...common_exclude);
+
+
 project.synth();
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,72 @@
-import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
+import * as path from 'path';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as s3 from '@aws-cdk/aws-s3';
+import { App, Construct, Stack, StackProps, CfnOutput } from '@aws-cdk/core';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    this.createCodePipelineEventLambdaFunction(
+      'main',
+      '',
+      'pipeline.pipelineName',
+      '',
+    );
+  }
+
+  private createCodePipelineEventLambdaFunction(
+    stage: string,
+    slackWebhookURL: string,
+    codePipelineName: string,
+    githubPersonalToken: string,
+  ): lambda.Function {
+    const badgeBucket = new s3.Bucket(this, 'BadgeBucket', {
+      publicReadAccess: true,
+    });
+
+    const badgeBucketImageKeyName = `${stage}-latest-build.svg`;
+
+    const lambdaFunc = new lambda.Function(this, 'CodepipelineEventLambda', {
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, '../lambda/codepipeline-event'),
+        {
+          bundling: {
+            user: 'root',
+            image: lambda.Runtime.NODEJS_14_X.bundlingImage,
+            command: [
+              'bash',
+              '-c',
+              [
+                'npm install',
+                'npm run build',
+                'cp -r /asset-input/dist /asset-output/',
+                'npm install --only=production',
+                'cp -a /asset-input/node_modules /asset-output/',
+              ].join(' && '),
+            ],
+          },
+        },
+      ),
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'dist/codepipelineEventLambda.handler',
+      environment: {
+        STAGE: stage,
+        SLACK_WEBHOOK_URL: slackWebhookURL,
+        BADGE_BUCKET_NAME: badgeBucket.bucketName,
+        BADGE_BUCKET_IMAGE_KEY_NAME: badgeBucketImageKeyName,
+        CODE_PIPELINE_NAME: codePipelineName,
+        GITHUB_PERSONAL_TOKEN: githubPersonalToken,
+      },
+    });
+
+    badgeBucket.grantReadWrite(lambdaFunc);
+
+    new CfnOutput(this, 'badgeMarkdownLink', {
+      value: `[![Build Status](https://${badgeBucket.bucketName}.s3-ap-northeast-1.amazonaws.com/${badgeBucketImageKeyName}#1)](https://ap-northeast-1.console.aws.amazon.com/codesuite/codepipeline/pipelines/${codePipelineName}/view)`,
+    });
+
+    return lambdaFunc;
   }
 }
 
@@ -20,3 +82,5 @@ new MyStack(app, 'my-stack-dev', { env: devEnv });
 // new MyStack(app, 'my-stack-prod', { env: prodEnv });
 
 app.synth();
+
+
