f2425c3996a17231db7258cc9349bba28d02b514
Chris Yang <kimisme9386@gmail.com>
2021-09-26 12:49:01 +0000

    fix: add directory check

--- .gitpod/oh-my-zsh.sh
+++ .gitpod/oh-my-zsh.sh
@@ -16,9 +16,12 @@ while [[ ! -d ~/.oh-my-zsh ]]; do
 done
 
 if [[ -d ~/.oh-my-zsh ]]; then
-    # install zsh plugin
-    git clone git://github.com/zsh-users/zsh-autosuggestions && \
-    sudo mv zsh-autosuggestions ~/.oh-my-zsh/custom/plugins  
+
+    if [[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
+        # install zsh plugin
+        git clone git://github.com/zsh-users/zsh-autosuggestions && \
+        sudo mv zsh-autosuggestions ~/.oh-my-zsh/custom/plugins  
+    fi
 
     # Change zsh theme and zsh plugin
     sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc && \
