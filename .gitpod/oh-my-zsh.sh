3c9712a727431a57e15d65600f8790d59f784930
Chris Yang <kimisme9386@gmail.com>
2021-09-26 11:12:21 +0000

    fix: fix syntax and add forloop

--- .gitpod/oh-my-zsh.sh
+++ .gitpod/oh-my-zsh.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
-if [[ ! -d "~/.oh-my-zsh" ]]; then
+if [[ ! -d ~/.oh-my-zsh ]]; then
     # Install Oh-My-Zsh
     wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 fi
 
-if [[ -d "~/.oh-my-zsh" ]]; then
+wait_time=0
+while [[ ! -d ~/.oh-my-zsh ]]; do 
+    if [[ ${wait_time} == 10 ]]; then
+        break
+    fi
+
+    sleep 1; 
+    wait_time=$((wait_time+1))
+done
+
+if [[ -d ~/.oh-my-zsh ]]; then
     # install zsh plugin
     git clone git://github.com/zsh-users/zsh-autosuggestions && \
     sudo mv zsh-autosuggestions ~/.oh-my-zsh/custom/plugins  
-fi
 
-# Change zsh theme and zsh plugin
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc && \
-sed -i 's/plugins=(git)/plugins=(zsh-autosuggestions)/' ~/.zshrc
+    # Change zsh theme and zsh plugin
+    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc && \
+    sed -i 's/plugins=(git)/plugins=(zsh-autosuggestions)/' ~/.zshrc
+fi
 
 if grep -q source /usr/share/bash-completion/completions/git ~/.zshrc; then
     echo "source /usr/share/bash-completion/completions/git" >> ~/.zshrc
