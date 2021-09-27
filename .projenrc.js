1df72e2619d5157004becc001eb9d41fbc6cabba
Chris Yang <kimisme9386@gmail.com>
2021-09-27 11:15:19 +0800

    feat: use projen to generate .gitpod.yml

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,9 @@
-const { AwsCdkTypeScriptApp, DependenciesUpgradeMechanism } = require('projen');
+const {
+  AwsCdkTypeScriptApp,
+  DependenciesUpgradeMechanism,
+  Gitpod,
+  DevEnvironmentDockerImage,
+} = require('projen');
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.123.0',
   cdkVersionPinning: true,
@@ -24,5 +29,43 @@ const common_exclude = [
 
 project.gitignore.exclude(...common_exclude);
 
+// gitpod
+const gitpodPrebuild = project.addTask('gitpod:prebuild', {
+  description: 'Prebuild setup for Gitpod',
+});
+gitpodPrebuild.exec('yarn install --frozen-lockfile --check-files');
+
+let gitpod = new Gitpod(project, {
+  dockerImage: DevEnvironmentDockerImage.fromFile('.gitpod.Dockerfile'),
+  prebuilds: {
+    addCheck: true,
+    addBadge: true,
+    addLabel: true,
+    branches: true,
+    pullRequests: true,
+    pullRequestsFromForks: true,
+  },
+});
+
+gitpod.addCustomTask({
+  name: 'install package and check zsh and zsh plugin',
+  init: `yarn gitpod:prebuild
+sudo chmod +x ./.gitpod/oh-my-zsh.sh && ./.gitpod/oh-my-zsh.sh`,
+});
+
+gitpod.addCustomTask({
+  name: 'run docker',
+  command: 'sudo docker-up &',
+});
+
+gitpod.addCustomTask({
+  name: 'change default shell to zsh and start zsh shell',
+  command: 'sudo chsh -s $(which zsh) && zsh',
+});
+
+/* spellchecker: disable */
+gitpod.addVscodeExtensions(
+  'dbaeumer.vscode-eslint',
+);
 
 project.synth();
